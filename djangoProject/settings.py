5990f4a93c695990ddc0c37ac152c75c8b3b2e25
Moe Jallow <jallowmoha@gmail.com>
2021-08-05 14:15:45 -0600

    update

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['scorpions-env.eba-6tmnvjme.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['scorpions-api.eba-9cxvrhbp.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
